3bf99dcca170bb88353df3befdc993a020005928
Mark Blackmore <mblackmo@yahoo.com>
2017-08-11 08:33:29 -0800

    Initial commit.

---
+++ Lecture Scratch Work.R
@@ -0,0 +1,11 @@
+# Coursera Data Science Specialization
+# Devleoping Data Products
+# Scratch Work
+##########################################################################################
+
+# Demo of manipulate function
+library(manipulate)
+manipulate(plot(1:x), x = slider(1,100))
+manipulate(hist(rnorm(1:x)), x = slider(1, 1000))
+
+
