774766e3122a9296060ac1b39671d8e0e49ccd43
Mark Blackmore <mblackmo@yahoo.com>
2017-08-11 17:53:58 -0800

    First Shiny app.

--- Lecture Scratch Work.R
+++ Lecture Scratch Work.R
@@ -8,4 +8,19 @@ library(manipulate)
 manipulate(plot(1:x), x = slider(1,100))
 manipulate(hist(rnorm(1:x)), x = slider(1, 1000))
 
+#########################################################################################
+# First Shiny app - starting from template
+ui <- fluidPage(
+  sliderInput(inputId = "num", 
+              label = "Choose an number",
+              value = 25, min = 1, max = 100),
+  plotOutput("hist")
+)
 
+server <- function(input, output) {
+  output$hist = renderPlot({
+    hist(rnorm(input$num))
+    })
+}
+
+shinyApp(ui = ui, server = server)
