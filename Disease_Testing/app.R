c592908b6b86a27b46beae9f5f1a8a7e0906d690
Mark Blackmore <mblackmo@yahoo.com>
2017-08-30 15:24:24 -0700

    Shiny App for Submission

--- Disease_Testing/app.R
+++ Disease_Testing/app.R
@@ -3,38 +3,46 @@
 # Course Project - Shiny Web App
 
 library(shiny)
+library(ggplot2)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
    
    # Application title
-   titlePanel("Disease Testing"),
+   titlePanel("Disease Screening Tests: After a test, what are the chances that you actually have a disease?"),
    
    # Sidebar with a slider input for number of bins 
    sidebarLayout(
       sidebarPanel(
-        h4("Prevalence: "),
-        numericInput("prev", "Disease Prevalence (%)", 
+        h4("Prevalence: Percentage in a population that have a certain disease"),
+        numericInput("prev", "Input: Prevalence (%)", 
                      min = 0, 
                      max = 100,
                      value = 9),
-        h3("add text"),
-        sliderInput("sens","Sensitivity (%):",
+        br(),
+        h4("Sensitivity: Diagnostic Test's True Positive Rate "),
+        sliderInput("sens","Input: Sensitivity (%):",
                      min = 0,
                      max = 100,
-                     value = 95),
-        h3("add text"),
-        sliderInput("spec","Specificity (%):",
+                     value = 41),
+        br(),
+        h4("Specificity: Diagnostic Test's True Neagative Rate "),
+        sliderInput("spec","Input: Specificity (%):",
                      min = 0,
                      max = 100,
-                     value = 95)
+                     value = 93),
+        h5("Example:"),
+        h5("Diabetes has a 9% prevalence, and a typical screening test has 41% sensitivity and 93% specificity"),
+        a("https://academic.oup.com/epirev/article/33/1/63/483202/Screening-for-Type-2-Diabetes-and-Dysglycemia")
       ),
       
       # Show a plot of the generated distribution
       mainPanel(
-        h3("Cases per 100,000 people"), 
+        strong(h3("Cases per 100,000 people")), 
         tableOutput("confusion"),
-        h3("Plot"),
+        br(),
+        h4("False Omission Rate (FOR) - Given a negative test, what is the probability that you have the condition"),
+        h4("Positive Predictive Value (PPV): Given a positive test, what is the probaility that you have the condition"),
         plotOutput("prediction")
       )
    )
@@ -55,7 +63,7 @@ server <- function(input, output) {
     FOR    <- FN / (FN + TN)
     
     
-    df <- data.frame(outcome = c("Test Postive", "Test Negative", "Total"),
+    df <- data.frame(Outcome = c("Test Postive", "Test Negative", "Total"),
       Disease = c(TP, FN, totDis), 
       No_Disease = c(FP, TN, totNodis),
       Total = c(TP+FP, TN+FN, totDis + totNodis))
@@ -71,14 +79,14 @@ server <- function(input, output) {
     TN     <- floor(totNodis * input$spec/100)
     FP     <- totNodis - TN
     
-    df <- data.frame(outcome = c("Test Postive", "Test Negative", "Total"),
+    df <- data.frame(Outcome = c("Test Postive", "Test Negative", "Total"),
                      Disease = c(TP, FN, totDis), 
                      No_Disease = c(FP, TN, totNodis),
                      Total = c(TP+FP, TN+FN, totDis + totNodis))
-    df2 <- data.frame(outcome = c("PPV", "FOR", "Prevalence"),
-                      metric = 100 * df$Disease/df$Total)
+    df2 <- data.frame(Outcome = c("PPV", "FOR", "Prevalence"),
+                      metric = round(100 * df$Disease/df$Total,2))
     
-    ggplot(df2, aes(x = outcome, y = metric)) + geom_bar(stat = "identity", fill="steelblue")+
+    ggplot(df2, aes(x = Outcome, y = metric)) + geom_bar(stat = "identity", fill="steelblue")+
       geom_text(aes(label = metric), vjust=-0.3, size = 4) +
       theme_minimal()
     
