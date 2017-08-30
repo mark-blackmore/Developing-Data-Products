9471ef8bc3d76b728707dfb429ce1af552e6b64d
Mark Blackmore <mblackmo@yahoo.com>
2017-08-30 16:19:07 -0700

    Deployed Shiny Application

--- Disease_Testing/app.R
+++ Disease_Testing/app.R
@@ -9,40 +9,41 @@ library(ggplot2)
 ui <- fluidPage(
    
    # Application title
-   titlePanel("Disease Screening Tests: After a test, what are the chances that you actually have a disease?"),
+   titlePanel(h2("Disease Screening Tests: What are the chances that you actually have a disease?")),
    
    # Sidebar with a slider input for number of bins 
    sidebarLayout(
       sidebarPanel(
-        h4("Prevalence: Percentage in a population that have a certain disease"),
-        numericInput("prev", "Input: Prevalence (%)", 
+        numericInput("prev", "Prevalence (%): Percent in population that have a certain disease", 
                      min = 0, 
                      max = 100,
                      value = 9),
-        br(),
-        h4("Sensitivity: Diagnostic Test's True Positive Rate "),
-        sliderInput("sens","Input: Sensitivity (%):",
+        
+      
+        sliderInput("sens","Sensitivity (%): Test's True Positive Rate:",
                      min = 0,
                      max = 100,
                      value = 41),
-        br(),
-        h4("Specificity: Diagnostic Test's True Neagative Rate "),
-        sliderInput("spec","Input: Specificity (%):",
+        
+
+        sliderInput("spec","Specificity (%): Test's True Neagative Rate",
                      min = 0,
                      max = 100,
                      value = 93),
+       
         h5("Example:"),
         h5("Diabetes has a 9% prevalence, and a typical screening test has 41% sensitivity and 93% specificity"),
-        a("https://academic.oup.com/epirev/article/33/1/63/483202/Screening-for-Type-2-Diabetes-and-Dysglycemia")
+        a(href = "https://academic.oup.com/epirev/article/33/1/63/483202/
+          Screening-for-Type-2-Diabetes-and-Dysglycemia", "source")
       ),
       
       # Show a plot of the generated distribution
       mainPanel(
-        strong(h3("Cases per 100,000 people")), 
+        strong(h4("Cases per 100,000 people")), 
         tableOutput("confusion"),
-        br(),
-        h4("False Omission Rate (FOR) - Given a negative test, what is the probability that you have the condition"),
-        h4("Positive Predictive Value (PPV): Given a positive test, what is the probaility that you have the condition"),
+      
+        h5("False Omission Rate (FOR) - Given a negative test, what is the probability that you have the condition"),
+        h5("Positive Predictive Value (PPV): Given a positive test, what is the probaility that you have the condition"),
         plotOutput("prediction")
       )
    )
